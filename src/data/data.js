8325887c9c8c4624f54832595e97a61a022fc1dd
Sophia nabil <sophianabil5@gmail.com>
2025-09-18 14:00:50 +0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Dosa from "../images/dosa.jpg";
+import egges from "../images/egges.jpg";
+import spicySoup from "../images/soup spaicy.jpg";
+import saladwithbread from "../images/salad with bread.jpg";
+import pasta from "../images/pasta.jpg";
+import meat from "../images/meat.jpg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 300,
+  },
+  {
+    name: "egges",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: egges,
+    price: 250,
+  },
+  {
+    name: "spaicy soup",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: spicySoup,
+    price: 300,
+  },
+  {
+    name: "salad with bread",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: saladwithbread,
+    price: 100,
+  },
+  {
+    name: "pasta",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: pasta,
+    price: 400,
+  },
+  {
+    name: "beef Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: meat,
+    price: 500,
+  },
+];
